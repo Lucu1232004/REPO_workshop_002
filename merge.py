1a418d7b22ad5bc4ee1169dea908ed25f1e46e75
Lucu1232004 <samuel.patino@uao.edu.co>
2023-10-14 20:37:17 -0500

    WKSHP2

--- merge.py
+++ merge.py
@@ -1,93 +1,87 @@
 import pandas as pd
-import json
 import mysql.connector
+import numpy as np
 
-def merge(**kwargs):
-    ti = kwargs["ti"]
-    str_data_csv = ti.xcom_pull(task_ids='transform_csv')
-    json_data_csv = json.loads(str_data_csv)
-    data_csv = pd.json_normalize(data=json_data_csv)
-
-    ti = kwargs["ti"]
-    str_data_db = ti.xcom_pull(task_ids='transform_db')
-    json_data_db = json.loads(str_data_db)
-    data_db = pd.json_normalize(data=json_data_db)
-
-    dataset_final = data_db.merge(data_csv, how='inner', 
-                            left_on=['nominee', 'artist'], 
-                            right_on=['track_name', 'artists'])
-    
-    return dataset_final.to_json(orient='records')
-
-def load(**kwargs):
-    ti = kwargs["ti"] 
-    str_data=ti.xcom_pull(task_ids='load') 
-    json_data_db = json.loads(str_data)
-    transform_csv= pd.json_normalize(data=json_data_db)
-
+def create_connection():
     cnx = mysql.connector.connect(
         user='root',
         password='Octubre03',
         host='localhost',
         database='workshop_02'
     )
+    return cnx
 
-    create_table_query = """
-    CREATE TABLE Merge (
-        year INT,
-        nominee VARCHAR(255),
-        artist VARCHAR(255),
-        winner BOOLEAN,
-        album_name VARCHAR(255),
-        track_name VARCHAR(255),
-        popularity INT,
-        track_genre VARCHAR(255),
-        duration_seconds DECIMAL(10, 2)
-    );
-    """
 
-    try:
-        cursor = cnx.cursor()
-        cursor.execute(create_table_query)
-        cnx.commit()
-        print("Table 'Merge' created successfully.")
-    except mysql.connector.Error as err:
-        print(f"Error creating table: {err}")
-    finally:
-        cursor.close()
+create_table_query = """
+CREATE TABLE Merge (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    winner BOOLEAN,
+    album_name VARCHAR(1000),
+    track_name VARCHAR(1000),
+    popularity INT,
+    track_genre VARCHAR(1000),
+    duration_ms DECIMAL(10, 2)
+);
+"""
 
-    insert_query = """
-    INSERT INTO Merge (year, nominee, artist, winner, album_name, track_name, popularity, track_genre, duration_seconds)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
-    
-    try:
-        cursor = cnx.cursor()
-        dataset_final_json = merge()  
+insert_query = """
+INSERT INTO Merge (winner, album_name, track_name, popularity, track_genre, duration_ms)
+VALUES (1, %s, %s, %s, %s, %s)
+"""
 
-        dataset_final = json.loads(dataset_final_json)
+file1 = "spotify_dataset.csv"
+file2 = "the_grammy_awards.csv"
 
-        print("Data to be inserted:")
-        print(dataset_final)
+df1 = pd.read_csv(file1, encoding='utf-8')
+df2 = pd.read_csv(file2, encoding='utf-8')
 
-        for row in dataset_final:
-            values = (
-                row['year'], row['nominee'], row['artist'], row['winner'], row['album_name'],
-                row['track_name'], row['popularity'], row['track_genre'], row['duration_seconds']
-            )
-            cursor.execute(insert_query, values)
-        cursor.close()
-        cnx.commit()
-        print("Data inserted into the 'Merge' table successfully.")
-    except (Exception, mysql.connector.Error) as error:
-        print(f"Error inserting data: {error}")
-    finally:
+combined_df = pd.concat([df1, df2], ignore_index=True)
+
+combined_df = combined_df.replace({"Unknown": None, np.nan: None})
+
+default_values = {
+    'album_name': '',
+    'track_name': '',
+    'popularity': 0,
+    'track_genre': '',
+    'duration_ms': 0
+}
+combined_df.fillna(default_values, inplace=True)
+
+cnx = create_connection()
+
+try:
+    cursor = cnx.cursor()
+
+    cursor.execute(create_table_query)
+    cnx.commit()
+    print("Tabla 'Merge' creada exitosamente.")
+
+    for index, row in combined_df.iterrows():
+        duration_ms = None
+        if not pd.isna(row['duration_ms']):
+            try:
+                duration_ms = float(row['duration_ms'])
+            except ValueError:
+                duration_ms = None
+
+        values = (
+            row['album_name'],
+            row['track_name'],
+            int(row['popularity']) if not pd.isna(row['popularity']) else None,
+            row['track_genre'],
+            duration_ms
+        )
+
+        cursor.execute(insert_query, values)
+
+    cnx.commit()
+    print("Datos insertados en la tabla 'Merge' exitosamente con 'winner' = 1.")
+except (Exception, mysql.connector.Error) as error:
+    print(f"Error: {error}")
+finally:
+    if cnx:
         cnx.close()
-
-load()
-
-
-
 
 
 
